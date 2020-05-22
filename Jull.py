3e21efb1b86c6581b764b757a1d5ee1018aa859b
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-23 03:41:43 +0700

    Update and rename Jull.sh to Jull.py

---
+++ Jull.py
@@ -0,0 +1,59 @@
+#!/bin/python
+#tools by cyber limit
+#recode mati ema lu
+import os
+import sys
+
+sleep 1
+os.system('clear')
+print "MASUKIN NAMA KAMU : "
+raw_input Nama :
+sleep 2
+print "PASSWORD TOOLS : "
+raw_password :limit
+print "BERHASIL"
+'clear'
+figlet CyberLimit
+print ""
+  print "✓===============================✓"
+  print "|     Autor : Cyber Limit       |"
+  print "|    My tim : Wisnu | panjul    |"
+  print "|   youtube : Cyber Limit       |"
+  print "✓===============================✓"
+  print ""
+print "Note : Hargai Yang Membuat Tools!!"
+print
+        print "[+]PILIH YANG ADA AJA TAIK"
+print ""
+print "1.>CEK IP "
+sleep 1
+print "2.>Hack Fb mbf "
+slepp 1
+print "Pilih : "
+raw_input Pilih :
+if pilih "1" :
+        os.system('git clone https://github.com/Bl4ckDr460n/My-Ip')
+        print "[+]penginstallan selesai"
+        sleep 1
+        print "ketik : cd My-Ip"
+        sleep 1
+        print "Lanjut ketik : python2 My-Ip.py"
+elif pilih "2" 
+       os.system('git clone https:github.com/cyberlimit/darkcyber')
+       print "[+]pengisntallan selesai"
+       sleep 1
+       print "ketik : cd darkcyber"
+       sleep 1
+       print "Lanjut ketik : python2 Limit.py"
+
+       
+       
+       
+       
+       
+
+
+
+
+
+
