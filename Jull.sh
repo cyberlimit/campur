f593beac8743fcb56466b01dc8e6dd1d5bce32d5
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-23 02:59:09 +0700

    Create Jull.sh

---
+++ Jull.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#tools by cyber limit
+#recode mati ema lu
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+sleep 1
+clear
+echo -e "\e[1;34mMASUKIN NAMA KAMU : "
+read NAMA
+sleep 2
+echo "PASSWORD TOOLS : "
+read password :limit
+echo "BERHASIL"
+clear
+figlet CyberLimit
+echo ""
+  echo   -e "\e[1;31m✓===============================>
+  echo   -e "\e[1;34m|     Autor : Cyber Limit       >
+  echo   "|    My tim : Wisnu | panjul      |"
+  echo   "|   youtube : Cyber Limit         |"
+  echo   -e "\e[1;31m✓===============================>
+  echo ""
+echo -e "\e[1;34mNote : Hargai Yang Membuat Tools!!"
+echo ""
+echo ""
+        echo -e "\e[1;33mPILIH YANG ADA AJA TAIK"
+echo ""
+echo "1.>CEK IP "
+echo "2.>Hack Fb mbf "
+echo "3.>Scan IP web "
